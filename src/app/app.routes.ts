60024fbf3ca84b176c782636cd6d163fa44cd741
Costainde <marcomacchia2397@gmail.com>
2025-03-22 12:02:34 +0100

    Start project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
+import { ContattiComponent } from './contatti/contatti.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'chi-siamo', component: ChiSiamoComponent },
+  { path: 'contatti', component: ContattiComponent },
+];
